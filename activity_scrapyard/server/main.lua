f8aab0845f26819ee4216d8b88567f1f1d783ad6
Alex Fowler <alex@mpfowler.com>
2021-04-21 04:25:42 -0400

    assigning players to scrapyards now random

--- activity_scrapyard/server/main.lua
+++ activity_scrapyard/server/main.lua
@@ -2,20 +2,43 @@ local activityName = Config.activity_name
 local activityAvailable = true
 
 function AssignYard(_playerServerId)
+	local activeYards = {}
+	local numYards = 0
 	for k,yard in pairs(scrapYards) do
+		if yard.active then
+			table.insert(activeYards, yard)
+			numYards = numYards + 1
+		end
+	end
+	while numYards > 0 do
+		local index = math.random(1, numYards)
+		local yard = activeYards[index]
+		local id = yard.id
 		local nWorkers = 0
 		for k in pairs(yard.workers) do
 			nWorkers = nWorkers + 1
 		end
 		if nWorkers < yard.maxWorkers then
 			TriggerClientEvent(("%s:assignYard"):format(activityName), _playerServerId, _playerServerId, yard)
-			table.insert(scrapYards[k].workers, _playerServerId)
+			AddWorker(id, _playerServerId)
+			print("added")
 			return
+		else
+			table.remove(activeYards, index)
+			numYards = numYards - 1
 		end
 	end
 	-- No available yards
 	local failMessage = Config.yard_assign_fail_message
-	ActivityFailed(failMessage)
+	exports["np-activities"]:activityCompleted(activityName, _playerServerId, false, failMessage)
+end
+
+function AddWorker(_yardId, _playerServerId)
+	for k,yard in pairs(scrapYards) do
+		if yard.id == _yardId then
+			table.insert(scrapYards[k].workers, _playerServerId)
+		end
+	end
 end
 
 -- Calling this will assign desired player to a scrapyard and start the job
@@ -33,10 +56,33 @@ exports('setActivityStatus', function(_enabled)
 	activityAvailable = _enabled
 end)
 
--- == Debug ==
-Citizen.CreateThread(function()
-	Citizen.Wait(50)
-	exports["np-activities"]:activityInProgress(activityName, 1)
-
-	AssignYard(1)
+-- Calling this will set the yard with id _locationId to active/inactive
+exports('setLocationStatus', function(_locationId, _enabled)
+	for k,yard in pairs(scrapYards) do
+		if yard.id == _locationId then
+			scrapYards[k].active = _enabled
+		end
+	end
 end)
+
+RegisterNetEvent(("%s:jobEnded"):format(activityName))
+AddEventHandler(("%s:jobEnded"):format(activityName), function(_playerServerId)
+	for k,yard in pairs(scrapYards) do
+		for j,player in pairs(yard.workers) do
+			if player == _playerServerId then
+				table.remove(scrapYards[k].workers, j)
+				return
+			end
+		end
+	end
+end)
+
+-- == Debug ==
+if Config.debug_mode then -- Starts job automatically
+	Citizen.CreateThread(function()
+		Citizen.Wait(50)
+		exports["np-activities"]:activityInProgress(activityName, -1)
+
+		AssignYard(-1)
+	end)
+end
